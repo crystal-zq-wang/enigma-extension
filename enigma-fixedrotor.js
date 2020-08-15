730269e3c98392b46489ac2641c8a1fc84e9e81c
Crystal Wang <crystalwang@berkeley.edu>
2020-08-15 12:47:19 -0700

    changes/conslidating

--- enigma-fixedrotor.js
+++
@@ -1,8 +0,0 @@
-class FixedRotor extends Rotor {
-
-    /** A non-moving rotor named NAME whose permutation at the 0 setting
-     * is given by PERM. */
-    constructor(name, perm) {
-        super(name, perm);
-    }
-}
